fa3f06e38ef667729badeb558cca2d97908b609a
Kurt Robin Antonio <72644734+krla22@users.noreply.github.com>
2025-01-31 20:34:36 +0800

    Personal Project Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '@/styles/global.css';
+import Head from "next/head";
+import "@/styles/global.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>The Relationship Test</title>
+        <meta name="description" content="The magic of knowing your relationship!" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
